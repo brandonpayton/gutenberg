79d07fc81746145e8a10d0e6cba13a98e4c52c8c
iseulde <ella@iseulde.com>
2017-10-18 23:46:51 +0200

    Add vertical arrow navigation for blocks

--- editor/utils/dom.js
+++ editor/utils/dom.js
@@ -1,17 +1,22 @@
 /**
- * Check whether the selection touches an edge of the container
- *
- * @param  {Element} container DOM Element
- * @param  {Boolean} start     Reverse means check if it touches the start of the container
- * @return {Boolean}           Is Edge or not
+ * External dependencies
  */
-export function isEdge( container, start = false ) {
-	if ( [ 'INPUT', 'TEXTAREA' ].indexOf( container.tagName ) !== -1 ) {
+import { includes } from 'lodash';
+
+/**
+ * Check whether the caret is horizontally at the edge of the container.
+ *
+ * @param  {Element} container Focusable element.
+ * @param  {Boolean} isReverse Set to true to check left, false for right.
+ * @return {Boolean}           True if at the edge, false if not.
+ */
+export function isHorizontalEdge( container, isReverse ) {
+	if ( includes( [ 'INPUT', 'TEXTAREA' ], container.tagName ) ) {
 		if ( container.selectionStart !== container.selectionEnd ) {
 			return false;
 		}
 
-		if ( start ) {
+		if ( isReverse ) {
 			return container.selectionStart === 0;
 		}
 
@@ -24,21 +29,22 @@ export function isEdge( container, start = false ) {
 
 	const selection = window.getSelection();
 	const range = selection.rangeCount ? selection.getRangeAt( 0 ) : null;
-	const position = start ? 'start' : 'end';
-	const order = start ? 'first' : 'last';
-	const offset = range[ `${ position }Offset` ];
-
-	let node = range.startContainer;
 
 	if ( ! range || ! range.collapsed ) {
 		return false;
 	}
 
-	if ( start && offset !== 0 ) {
+	const position = isReverse ? 'start' : 'end';
+	const order = isReverse ? 'first' : 'last';
+	const offset = range[ `${ position }Offset` ];
+
+	let node = range.startContainer;
+
+	if ( isReverse && offset !== 0 ) {
 		return false;
 	}
 
-	if ( ! start && offset !== node.textContent.length ) {
+	if ( ! isReverse && offset !== node.textContent.length ) {
 		return false;
 	}
 
@@ -56,18 +62,77 @@ export function isEdge( container, start = false ) {
 }
 
 /**
- * Places the caret at start or end of a given element
+ * Gets vertical edge information:
+ *   * isEdge: Whether the caret is at the vertical edge of the container.
+ *   * rect: Dimensions of the caret when available.
  *
- * @param  {Element} container DOM Element
- * @param  {Boolean} start     Position: Start or end of the element
+ * @param  {Element} container Focusable element.
+ * @param  {Boolean} isReverse Set to true to check top, false for bottom.
+ * @return {Object}            Vertical edge information.
  */
-export function placeCaretAtEdge( container, start = false ) {
-	const isInputOrTextarea = [ 'INPUT', 'TEXTAREA' ].indexOf( container.tagName ) !== -1;
+export function getVerticalEdge( container, isReverse ) {
+	if ( includes( [ 'INPUT', 'TEXTAREA' ], container.tagName ) ) {
+		return {
+			isEdge: isHorizontalEdge( container, isReverse ),
+		};
+	}
 
-	// Inputs and Textareas
-	if ( isInputOrTextarea ) {
+	if ( ! container.isContentEditable ) {
+		return { isEdge: true };
+	}
+
+	const selection = window.getSelection();
+	const range = selection.rangeCount ? selection.getRangeAt( 0 ) : null;
+
+	if ( ! range || ! range.collapsed ) {
+		return { isEdge: false };
+	}
+
+	// Adjust for empty containers.
+	const rangeRect =
+		range.startContainer.nodeType === window.Node.ELEMENT_NODE
+		? range.startContainer.getBoundingClientRect()
+		: range.getClientRects()[ 0 ];
+
+	if ( ! rangeRect ) {
+		return { isEdge: false };
+	}
+
+	const buffer = rangeRect.height / 2;
+	const editableRect = container.getBoundingClientRect();
+
+	// Too low.
+	if ( isReverse && rangeRect.top - buffer > editableRect.top ) {
+		return {
+			rect: rangeRect,
+			isEdge: false,
+		};
+	}
+
+	// Too high.
+	if ( ! isReverse && rangeRect.bottom + buffer < editableRect.bottom ) {
+		return {
+			rect: rangeRect,
+			isEdge: false,
+		};
+	}
+
+	return {
+		rect: rangeRect,
+		isEdge: true,
+	};
+}
+
+/**
+ * Places the caret at start or end of a given element.
+ *
+ * @param {Element} container Focusable element.
+ * @param {Boolean} isReverse True for end, false for start.
+ */
+export function placeCaretAtHorizontalEdge( container, isReverse ) {
+	if ( includes( [ 'INPUT', 'TEXTAREA' ], container.tagName ) ) {
 		container.focus();
-		if ( start ) {
+		if ( isReverse ) {
 			container.selectionStart = 0;
 			container.selectionEnd = 0;
 		} else {
@@ -77,14 +142,98 @@ export function placeCaretAtEdge( container, start = false ) {
 		return;
 	}
 
+	if ( ! container.isContentEditable ) {
+		container.focus();
+		return;
+	}
+
 	// Content editables
 	const range = document.createRange();
 	range.selectNodeContents( container );
-	range.collapse( start );
+	range.collapse( isReverse );
 	const sel = window.getSelection();
 	sel.removeAllRanges();
 	sel.addRange( range );
 	container.focus();
+}
+
+/**
+ * Polyfill.
+ * Get a collapsed range for a given point.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Document/caretRangeFromPoint
+ *
+ * @param  {Document} doc The document of the range.
+ * @param  {Float}    x   Horizontal position within the current viewport.
+ * @param  {Float}    y   Vertical position within the current viewport.
+ * @return {?Range}       The best range for the given point.
+ */
+function caretRangeFromPoint( doc, x, y ) {
+	if ( doc.caretRangeFromPoint ) {
+		return doc.caretRangeFromPoint( x, y );
+	}
+
+	if ( ! doc.caretPositionFromPoint ) {
+		return null;
+	}
+
+	const point = doc.caretPositionFromPoint( x, y );
+	const range = doc.createRange();
+
+	range.setStart( point.offsetNode, point.offset );
+	range.collapse( true );
+
+	return range;
+}
+
+/**
+ * Places the caret at the top or bottom of a given element.
+ *
+ * @param {Element} container           Focusable element.
+ * @param {Boolean} isReverse           True for bottom, false for top.
+ * @param {DOMRect} [rect]              The rectangle to position the caret with.
+ * @param {Boolean} [mayUseScroll=true] True to allow scrolling, false to disallow.
+ */
+export function placeCaretAtVerticalEdge( container, isReverse, rect, mayUseScroll = true ) {
+	if ( ! rect || ! container.isContentEditable ) {
+		placeCaretAtHorizontalEdge( container, isReverse );
+		return;
+	}
+
+	const buffer = rect.height / 2;
+	const editableRect = container.getBoundingClientRect();
+	const x = rect.left + ( rect.width / 2 );
+	const y = isReverse ? ( editableRect.bottom - buffer ) : ( editableRect.top + buffer );
+	const selection = window.getSelection();
+
+	// Temporary high z-index above toolbar.
+	// This is preferred over getting the toolbar node and set styles.
+	container.style.zIndex = '10000';
+
+	const range = caretRangeFromPoint( document, x, y );
+
+	container.style.zIndex = null;
+
+	if ( ! range || ! container.contains( range.startContainer ) ) {
+		if ( mayUseScroll ) {
+			// Might be out of view.
+			// Easier than attempting to calculate manually.
+			container.scrollIntoView( isReverse );
+			placeCaretAtVerticalEdge( container, isReverse, rect, false );
+			return;
+		}
+
+		placeCaretAtHorizontalEdge( container, isReverse );
+		return;
+	}
+
+	selection.removeAllRanges();
+	selection.addRange( range );
+	container.focus();
+	// Editable was already focussed, it goes back to old range...
+	// This fixes it.
+	selection.removeAllRanges();
+	selection.addRange( range );
 }
 
 /**
